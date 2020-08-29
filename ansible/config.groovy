85756ff95cc98192b7b63acc4176a9bbbbf624eb
Kevin Christopher <kevinchristopherkc7@gmail.com>
2020-08-29 18:34:09 +0530

    Pipeline Definition for Ansible

---
+++ ansible/config.groovy
@@ -0,0 +1,40 @@
+JOB_NAME = 'Ansible Setup'
+DISPLAY_NM = 'Configuration Project'
+CJK_GIT_URL = 'https://github.com/KevinChris7/terraform-ansible-aws.git'
+JOB_REPO = 'https://github.com/KevinChris7/trial-project.git'
+BRANCH_NAME = 'master'
+CREDENTIALS_ID = 'github'
+
+pipelineJob(JOB_NAME){
+    displayName(DISPLAY_NM)
+    logRotator{
+        daysToKeep(30)
+        numToKeep(10)
+    }
+    parameters{
+        stringParam('CJK_GIT_URL',CJK_GIT_URL)
+        stringParam('BRANCH_NAME', BRANCH_NAME)
+    }
+    definition{
+        cpsScm{
+            scm{
+                git{
+                    remote{
+                        credentials(CREDENTIALS_ID)
+                        url(JOB_REPO)
+                    }
+                    branch("*/master")
+                }
+            scriptPath('trial-project/ansible/Jenkinsfile')
+            lightweight()
+            }
+        }
+    }
+    properties{
+        rebuild{
+            autoRebuild(false)
+            rebuildDisabled(false)
+        }
+    }
+
+}
